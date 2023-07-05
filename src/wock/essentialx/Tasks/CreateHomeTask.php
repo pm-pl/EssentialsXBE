c836ae2d7b7f7521e5ab4a40a78f106332155da6
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-07-04 18:54:09 -0700

    Create CreateHomeTask.php

---
+++ src/wock/essentialx/Tasks/CreateHomeTask.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace wock\essentialx\Tasks\Homes;
+
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use wock\essentialx\Utils\DatabaseConnection;
+
+class CreateHomeTask extends AsyncTask {
+
+    private string $playerUuid;
+
+    private string $homeName;
+
+    private float $x;
+
+    private float $y;
+
+    private float $z;
+
+    private string $world;
+
+    public function __construct(string $playerUuid, string $homeName, float $x, float $y, float $z, string $world) {
+        $this->playerUuid = $playerUuid;
+        $this->homeName = $homeName;
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+        $this->world = $world;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function onRun(): void {
+        $database = new DatabaseConnection('db4free.net', 'startesting', 'startesting123', 'startesting', 3306);
+        $query = "INSERT INTO homes (player_uuid, home_name, x, y, z, world) VALUES (?, ?, ?, ?, ?, ?)";
+        $database->execute($query, [$this->playerUuid, $this->homeName, $this->x, $this->y, $this->z, $this->world]);
+    }
+}
