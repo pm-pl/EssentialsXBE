3c0e17adabfca61495a52ab029c2e15c564058a4
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-07-04 19:10:32 -0700

    Update RemoveHomeCommand.php

--- src/wock/essentialx/Commands/RemoveHomeCommand.php
+++ src/wock/essentialx/Commands/RemoveHomeCommand.php
@@ -6,11 +6,12 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use wock\essentialx\EssentialsX;
 use wock\essentialx\Managers\HomeManager;
 use wock\essentialx\Managers\WarpManager;
+use wock\essentialx\Tasks\RemoveHomeTask;
+use pocketmine\Server;
 
 class RemoveHomeCommand extends Command implements PluginOwned {
 
@@ -33,19 +34,22 @@ class RemoveHomeCommand extends Command implements PluginOwned {
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if (!$sender instanceof Player) {
-            $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
+            $sender->sendMessage("This command can only be used in-game.");
             return false;
         }
 
         if (count($args) === 0) {
-            $sender->sendMessage(TextFormat::RED . "Usage: /removehome <home-name>");
+            $sender->sendMessage(TextFormat::RED . "Usage: /delhome <home-name>");
             return false;
         }
 
         $homeName = $args[0];
         $player = $sender;
 
-        $this->homeManager->removeHome($player, $homeName);
+        Server::getInstance()->getAsyncPool()->submitTask(new RemoveHomeTask($player->getUniqueId()->toString(), $homeName));
+
+        $sender->sendMessage(TextFormat::RED . "Successfully removed home '{$homeName}'!");
+
         return true;
     }
 
